643c3ad259868ad1517ad6f93fce1935330e062a
maja-switaj <marina34.1998@gmail.com>
2024-09-28 22:33:31 +0200

    Backend changes p.2

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,39 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
-import openai
+from openai import OpenAI
+import json
+import requests
+import time
+from openai import AzureOpenAI
+
 
 app = Flask(__name__)
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
 load_dotenv()
+client = AzureOpenAI(
+  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
+  api_key= os.getenv("AZURE_OPENAI_API_KEY"),
+  api_version="2024-05-01-preview"
+)
+
 
 @app.route("/chat/completions", methods=['POST'])
 def connect():
     data = request.json
-    prompt = data.get('prompt', '')
     model = data.get('model','')
+    messages = data.get('messages','')
 
-    if not prompt:
-        return jsonify({'error': 'No prompt provided'}), 400
 
     try:
-        response = openai.Completion.create(
+        response = client.chat.completions.create(
             model=model,  
-            prompt=prompt,
-            max_tokens=100
+            messages=messages
         )
 
         return response
-        
-        
+
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
